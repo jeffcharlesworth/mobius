fdd0ac9ceda65afb8f5878db38cb4faa030d219d
Jeff Charlesworth <jcharlesworth02@gmail.com>
2017-09-01 15:00:40 -0500

    added method to call all transactions for current user

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,6 +3,7 @@ class Transaction < ActiveRecord::Base
   validates :to_user_id, presence: true
   before_create :balance_check
   after_create :update_balance
+  belongs_to :user
 
   private
   def update_balance
@@ -15,6 +16,7 @@ class Transaction < ActiveRecord::Base
   end
 
   def balance_check
+    raise ActiveRecord::Rollback, "Number must be greater than 0" if self.num_credits < 0
     if self.from_user_id
       sending_user = User.find_by_id(self.from_user_id)
        if sending_user.balance < self.num_credits
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :transactions
+  has_many :transactions, :primary_key => :to_user_id
   after_create :initial_credits
 
   validates :email, length: { minimum: 3, maximum: 254 }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+
+  def all_transactions
+    Transaction.where("to_user_id = ? OR from_user_id = ?", self.id, self.id).order("created_at DESC")
+  end
 
   private
   def initial_credits
